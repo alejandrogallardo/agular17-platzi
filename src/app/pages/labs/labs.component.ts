9bd30e20cc1ec50a67259a91b26fdaa9dba3eec1
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2024-01-13 15:55:19 -0600

    Event Binding

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  
+  welcome = 'Hola';
+  tasks = [
+    'Tarea 1',
+    'Tarea 2',
+    'Tarea 3'
+  ]
+
+  clickHandle() {
+    console.log("putos");
+    
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+    
+  }
+
+}
