de09c628865e0cfba06ae79f8c0b39805a836dcc
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2024-01-13 16:15:04 -0600

    keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -26,5 +26,10 @@ export class LabsComponent {
     console.log(event);
     
   }
+  
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 
 }
